beaf5d9919099d7edbc4f9df7d2fb9dc685f04b8
A5b84 <42934711+A5b84@users.noreply.github.com>
2026-03-26 21:31:51 +0100

    Update to 26.1

--- src/client/java/io/github/a5b84/example/config/ModMenuIntegration.java
+++ src/client/java/io/github/a5b84/example/config/ModMenuIntegration.java
@@ -2,12 +2,12 @@ package io.github.a5b84.example.config;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.AutoConfigClient;
 
 public class ModMenuIntegration implements ModMenuApi {
 
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return parent -> AutoConfig.getConfigScreen(ExampleModConfig.class, parent).get();
+        return parent -> AutoConfigClient.getConfigScreen(ExampleModConfig.class, parent).get();
     }
 }
